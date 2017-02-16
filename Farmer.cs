d0ad49f8636f5b24747583765d1aba1f88034291
raresJM <rares.scarlat.jm@gmail.com>
2017-02-17 00:22:42 +0200

    created the project Farmer

---
+++ Farmer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Farmer
+{
+    [TestClass]
+    public class Farmer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
