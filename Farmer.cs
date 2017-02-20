3accdd35e5dab098025d131051f2ee4ac8a5d9ef
raresJM <rares.scarlat.jm@gmail.com>
2017-02-20 23:28:42 +0200

    refactoring Farmer problem

--- Farmer.cs
+++ Farmer.cs
@@ -11,15 +11,23 @@ namespace Farmer
         {
             Assert.AreEqual(565.64, calculateResult());
         }
-
-
-
-
-        public double calculateResult()
+        [TestMethod]
+        public void TestResult_newWidth_10_finalArea_11()
         {
-            double result, result1, result2 = 0;
-            result1 = (-230 + Math.Sqrt(230 * 230 - 4 * 2 * (-770000)))/4;
-            result2 = (-230 - Math.Sqrt(230 * 230 - 4 * 2 * (-770000)))/4;
+            Assert.AreEqual(11, calculateResult(10,11));
+        }
+
+
+
+
+        //refactored method to accept parameters: 
+        // length - square length, 
+        // b -   width of the adjacent landplot
+        public double calculateResult(double newWidth, double finalArea)
+        {
+            double result, result1;
+            result1 = (-width + Math.Sqrt(width * width - 4 * 2 * (-770000)))/4;
+            result2 = (-width - Math.Sqrt(width * width - 4 * 2 * (-770000)))/4;
 
             if (result1 > 0)
             {
